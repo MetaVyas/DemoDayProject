9d1c287dafe28acf14d3037ae26d351a8a689796
pablomabloo <oringinalatbest@gmail.com>
2021-08-18 14:29:32 -0400

    new

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
-let titleBack;
-let font;
-let smokey;
-let title;
-let campfire;
+var titleBack;
+var font;
+var smokey;
+var title;
+var campfire;
+
+
+var swch = 0;
+var smokeyX = 200;
+var smokeyY = 200;
+var score = 0;
+var fires = [];
+var interval = 5000;
+
 
 function setup() {
-    let canvas = createCanvas(500, 500);
+    var canvas = createCanvas(500, 500);
     canvas.parent("smokeyGame");
     background(255, 255, 255);
     titleBack = loadImage("images/grassbackground.png");
@@ -13,48 +22,62 @@ function setup() {
     font = loadFont("assets/pixelated.ttf");
     smokey = loadImage("assets/smokey.gif");
     campfire = loadImage("assets/campfire.gif");
- }
 
- class theFire{
-     constructor(){
-        this.fireX = 50+451*Math.floor(Math.random());
-        this.fireY = 50+451*Math.floor(Math.random());
+    setInterval(function(){ append(fires, new theFire()); }, interval);
+    setInterval(function(){ subtractTime(interval); }, 10000);
+
+  }
+  function subtractTime(){
+    if(interval/1.5>=1000){
+      interval/=1.5;
     }
-    drawFire(){
-       image(campfire.gif,this.fireX,this.fireY);
+    else{
+      interval = 1000;
     }
-    }
- 
- let swch = 0;
- let smokeyX = 200;
- let smokeyY = 200;
- let score = 0;
-let fires = new theFire();
-console.log(fires);
+  }
   
- function draw() {
-     textFont(font);
-    if(swch != 0){
-    image(titleBack,0,0)
-    image(smokey,smokeyX,smokeyY,100,100);
+      class theFire{
+        constructor(){
+           this.fireX = 50+Math.floor(401*Math.random());
+           this.fireY = 50+Math.floor(401*Math.random());
+       }
+       drawFire(){
+         console.log(this);
+          image(campfire,this.fireX,this.fireY);
+       }
+       
+      }
+      
+      
+      
+      function draw() {
+
+        textFont(font);
+        
+        if(swch != 0){
+          image(titleBack,0,0);
+        for(var l = 0; l< fires.length; l++){
+          fires[l].drawFire();
+        }
+
     if ((keyIsDown(LEFT_ARROW)||keyIsDown(65))&&smokeyX>=5) {
         smokeyX -= 5;
       }
-    
+      
       if ((keyIsDown(RIGHT_ARROW)||keyIsDown(68))&&smokeyX<=400) {
         smokeyX += 5;
       }
-    
+      
       if ((keyIsDown(UP_ARROW)||keyIsDown(87))&&smokeyY>=5) {
         smokeyY -= 5;
       }
-    
+      
       if ((keyIsDown(DOWN_ARROW)||keyIsDown(83))&&smokeyY<=400) {
         smokeyY += 5;
       }
-   fires.drawFire();
-    
-    fill(0);
+      image(smokey,smokeyX,smokeyY,100,100);
+      
+      fill(0);
     rect(0,0,50,50);
     fill(255);
     textSize(15);
